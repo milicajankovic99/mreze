7e63a9b3df73cacc99a61ccbbb1e0a637fc86ab4
Milica Jankovic <milica.mica.jankovic.99@gmail.com>
2020-04-29 00:14:43 +0200

    drugi razvojni ciklus

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,32 @@
 from django.http import HttpResponse
 
 
+from django.template import loader
+
+from .models import Question
+
+from django.http import Http404
+
+from .models import Question
+
+from django.shortcuts import get_object_or_404, render
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
 def index(request):
-    
-    return HttpResponse("Ccao ja sam Milica")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# Leave the rest of the views (detail, results, vote) unchanged
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
